9b385cf2e13fe4d68fc88bb7aef1a0315d416265
Peter Ordonez <p.ordonez@infinityenergyorganisation.com>
2022-12-11 20:43:07 +0800

    Update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -59,9 +59,7 @@ class LoginController extends Controller
             return redirect('/dashboard');
         } elseif($type == 1) {
             return redirect('/');
-        } elseif($type == 2) {
-            return redirect('/company/dashboard');
-        }else {
+        } else {
             return redirect('/login')->with('error', 'Incorrect username or password');
         }
     }
