d3ca29d10713636d8e4b884c041c5d18c8341635
Charlie Macaraeg <charlie@digitalpie.co.nz>
2024-05-22 15:08:38 +0800

    Update

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -130,7 +130,7 @@ class CompanyController extends Controller
 
     public function applicants()
     {
-        $applicants = Jobs::select('jobs.*', 'applications.*', 'users.*')->leftJoin('applications', 'jobs.id', '=', 'applications.job_id')->leftJoin('users', 'applications.user_id', '=', 'users.id')->get();
+        $applicants = Jobs::select('jobs.*', 'applications.*', 'users.*')->leftJoin('applications', 'jobs.id', '=', 'applications.job_id')->leftJoin('users', 'applications.user_id', '=', 'users.id')->where('jobs.company', Auth::user()->name)->get();
         return view('admin.company_applicants', compact('applicants'));
     }
 
