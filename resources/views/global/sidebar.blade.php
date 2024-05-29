e788ff6464405c23c738137c32b1c591a3580108
Charlie Macaraeg <charlie@digitalpie.co.nz>
2024-05-29 09:53:38 +0800

    Update

--- resources/views/global/sidebar.blade.php
+++ resources/views/global/sidebar.blade.php
@@ -9,7 +9,7 @@
                 <li><a href="{{ route('companies') }}"><i class="mdi mdi-store"></i> <span>Companies</span></a></li>
                 <li><a href="{{ route('applicants') }}"><i class="mdi mdi-account-group"></i> <span>Applicants</span></a></li>
                 <li><a href="{{ route('users') }}"><i class="mdi mdi-account-group"></i> <span>Users</span></a></li>
-                @if(Auth::user()->user_role != 'guidance')
+                @if(Auth::user()->role == 0)
                 <li><a href="{{ route('admin.users') }}"><i class="mdi mdi-account-group"></i> <span>Admin Users</span></a></li>
                 @endif
             </ul>
